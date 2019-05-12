d5749e43a53b708d652ec54beca112370f2c16d3
Windsor Lima <windsorlimad@gmail.com>
2019-05-12 12:52:15 -0300

    Criação do model Usuario

--- pjrForca/src/view/Main.java
+++ pjrForca/src/view/Main.java
@@ -9,16 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DaoGenerico<Palavra> pG = new DaoGenerico<Palavra>();
-        Palavra p = new Palavra();
-        p.setDescricao("Dedos");
-        p.setDificuldade("Médio");
-
-        pG.saveOrUpdate(p);
-
-        System.out.println("XABLAU");
-        System.out.println("XABLAU");
-
     }
 
 }
